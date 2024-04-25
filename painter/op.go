1090379c53a70324d902d8571e77621016c98120
olhamish <olgami1402@gmail.com>
2024-04-25 04:04:59 +0300

    feat: implement the processing of the instructions

--- painter/op.go
+++ painter/op.go
@@ -1,9 +1,10 @@
 package painter
 
 import (
-	"image/color"
-
 	"golang.org/x/exp/shiny/screen"
+	"golang.org/x/image/draw"
+	"image"
+	"image/color"
 )
 
 // Operation змінює вхідну текстуру.
@@ -45,4 +46,45 @@ func WhiteFill(t screen.Texture) {
 // GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
 func GreenFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
+}
+
+func BlackFill(t screen.Texture) {
+	t.Fill(t.Bounds(), color.Black, screen.Src)
+}
+
+type Rectangle struct {
+	LeftPoint  image.Point
+	RightPoint image.Point
+}
+
+func (op *Rectangle) Do(t screen.Texture) bool {
+	t.Fill(image.Rect(op.LeftPoint.X, op.LeftPoint.Y, op.RightPoint.X, op.RightPoint.Y), color.Black, screen.Src)
+	return false
+}
+
+type Figure struct {
+	FigureCentralPos image.Point
+	Color            color.RGBA
+}
+
+func (op *Figure) Do(t screen.Texture) bool {
+	red := color.RGBA{255, 0, 0, 255}
+	hRect := image.Rect(op.FigureCentralPos.X-60, op.FigureCentralPos.Y-50, op.FigureCentralPos.X+60, op.FigureCentralPos.Y-30)
+	vRect := image.Rect(op.FigureCentralPos.X-10, op.FigureCentralPos.Y-50, op.FigureCentralPos.X+10, op.FigureCentralPos.Y+50)
+	t.Fill(hRect, red, draw.Src)
+	t.Fill(vRect, red, draw.Src)
+	return false
+}
+
+type Move struct {
+	NewPointCenter image.Point
+	Figures        []*Figure
+}
+
+func (op *Move) Do(t screen.Texture) bool {
+	for i := range op.Figures {
+		op.Figures[i].FigureCentralPos.X += op.NewPointCenter.X
+		op.Figures[i].FigureCentralPos.Y += op.NewPointCenter.Y
+	}
+	return false
 }
